013037b1cd7a4ff7fc66cbc93a1ff85a051ff16b
Muhammad Memon <142827381+Muhammad-Saad-Memon@users.noreply.github.com>
2025-04-04 22:03:05 -0500

    Update GameTermination.java

--- checkers/src/main/java/uta/cse3310/GameTermination/GameTermination.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/GameTermination.java
@@ -3,7 +3,10 @@ package uta.cse3310.GameTermination;
 import uta.cse3310.GameManager.Game;
 
 public class GameTermination {
-        //todo: create function that tells gameplay game is over
+        //function that tells gameplay game is over
+        public void endGame() {
+        
+        }
 
         //saves the results of the end of the match and put into database for leaderboard
         public void saveResults(Game player1, Game player2){
